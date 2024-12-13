b0f655b5a0c7fe5d1445eecbf023b5ce12531f9c
Seung Hyun Tark <makepin2r@gmail.com>
2024-12-13 22:35:31 +0900

    포맷팅 통일

--- jest.config.ts
+++ jest.config.ts
@@ -4,8 +4,8 @@ const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
   verbose: true,
-  moduleFileExtensions: ['ts', 'js'],
-  testMatch: ['**/tests/**/*.test.ts'], // 테스트 파일 경로 지정
+  moduleFileExtensions: ["ts", "js"],
+  testMatch: ["**/tests/**/*.test.ts"], // 테스트 파일 경로 지정
 };
 
 export default config;

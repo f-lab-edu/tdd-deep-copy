925e756af1feb92b4e602a6b1ea6f4f120ce482b
Seung Hyun Tark <makepin2r@gmail.com>
2024-12-13 22:14:05 +0900

    TypeScript 환경으로 변경

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+  moduleFileExtensions: ['ts', 'js'],
+  testMatch: ['**/tests/**/*.test.ts'], // 테스트 파일 경로 지정
+};
+
+export default config;

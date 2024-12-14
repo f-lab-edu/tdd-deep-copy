7d9389673eddc9730c3bbfe03a34f9d2e284aebd
Seung Hyun Tark <makepin2r@gmail.com>
2024-12-14 17:26:54 +0900

    중첩 객체 속성이 있는 객체의 깊은 복사 테스트

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 const deepCopy = (obj: any) => {
   const newObj = { ...obj };
+  for (const key in newObj) {
+    if (typeof newObj[key] === "object" && newObj[key] !== null) {
+      newObj[key] = deepCopy(newObj[key]);
+    }
+  }
   return newObj;
 };
 
--- tests/deepCopyObject.test.ts
+++ tests/deepCopyObject.test.ts
@@ -13,4 +13,37 @@ describe("객체 깊은 복사 테스트", () => {
     expect(copiedObject).toEqual(originalObject); // 원본 객체와 복사된 객체가 동일한 값을 가짐
     expect(copiedObject).not.toBe(originalObject); // 원본 객체와 복사된 객체가 동일한 객체가 아님
   });
+
+  it("중첩 객체 속성이 있는 객체의 깊은 복사", () => {
+    // 원본 객체
+    const originalObject = {
+      name: "John",
+      age: 30,
+      address: {
+        street: "123 Main St",
+        city: "New York",
+        country: "USA",
+      },
+    };
+
+    const copiedObject = deepCopy(originalObject);
+    expect(copiedObject).toEqual(originalObject); // 원본 객체와 복사된 객체가 동일한 값을 가짐
+    expect(copiedObject).not.toBe(originalObject); // 원본 객체와 복사된 객체가 동일한 객체가 아님
+    recursivelyCheckNestedProperties(originalObject, copiedObject);
+  });
 });
+
+const recursivelyCheckNestedProperties = (
+  originalObject: any,
+  copiedObject: any
+) => {
+  for (const key in originalObject) {
+    if (
+      typeof originalObject[key] === "object" &&
+      originalObject[key] !== null
+    ) {
+      expect(copiedObject[key]).toEqual(originalObject[key]);
+      expect(copiedObject[key]).not.toBe(originalObject[key]);
+    }
+  }
+};

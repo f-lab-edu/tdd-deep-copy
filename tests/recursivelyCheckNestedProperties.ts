3ade09973d1274f5894583c2e8f8225b7ab8ea3d
Seung Hyun Tark <makepin2r@gmail.com>
2024-12-25 15:52:23 +0900

    순환 참조, 함수 고려한 중첩 프로퍼티 검증 로직 추가

--- tests/recursivelyCheckNestedProperties.ts
+++ tests/recursivelyCheckNestedProperties.ts
@@ -1,15 +1,30 @@
 export const recursivelyCheckNestedProperties = (
   originalObject: any,
-  copiedObject: any
+  copiedObject: any,
+  seen = new WeakMap()
 ) => {
+  if (seen.has(originalObject)) {
+    expect(seen.get(originalObject)).toBe(copiedObject);
+    return;
+  }
+
+  seen.set(originalObject, copiedObject);
+
   for (const key in originalObject) {
-    if (
+    if (typeof originalObject[key] === "function") {
+      expect(copiedObject[key].toString()).toBe(originalObject[key].toString()); // fail
+    } else if (
       typeof originalObject[key] === "object" &&
       originalObject[key] !== null
     ) {
-      expect(copiedObject[key]).toEqual(originalObject[key]);
       expect(copiedObject[key]).not.toBe(originalObject[key]);
-      recursivelyCheckNestedProperties(originalObject[key], copiedObject[key]);
+      recursivelyCheckNestedProperties(
+        originalObject[key],
+        copiedObject[key],
+        seen
+      );
+    } else {
+      expect(copiedObject[key]).toBe(originalObject[key]);
     }
   }
 };
